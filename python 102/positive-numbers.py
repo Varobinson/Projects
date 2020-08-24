1b19536d7f20757eca209e3a4e98111f0c56d539
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-08-24 10:02:13 -0400

    positive2

---
+++ python 102/positive-numbers.py
@@ -0,0 +1,6 @@
+numbers = [2, 5, 9, 4, 1, 0]
+even_numbers = []
+
+for number in numbers:
+    if number > 0:
+        print(number)
---
+++ python 102/positive-numbers2.py
@@ -0,0 +1,7 @@
+numbers = [2, 5, 9, 4, 1, 0]
+even_numbers = []
+
+for number in numbers:
+    if number > 0:
+        even_numbers.append(number)
+print(even_numbers)
